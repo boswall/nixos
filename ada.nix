f5f0c19e53472a5d3cdd2836145ae3bd0cd7a789
Matt Rose <matt@glaikit.co.uk>
2025-01-04 15:05:09 +0000

    upgrade to 24.11

--- ada.nix
+++ ada.nix
@@ -134,7 +134,6 @@
   hardware.xpadneo.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
