f4e92be5ded7f19f6b0df3e5e068aa99dc6e4cd8
Matt Rose <matt@glaikit.co.uk>
2025-02-08 14:14:04 +0000

    add lm studio and obs

--- ada.nix
+++ ada.nix
@@ -259,6 +259,15 @@
       ];
     })
 
+    lmstudio
+
+    (pkgs.wrapOBS {
+      plugins = with pkgs.obs-studio-plugins; [
+        wlrobs
+        obs-backgroundremoval
+        obs-pipewire-audio-capture
+      ];
+    })
     mangohud
     steam-run
     heroic
